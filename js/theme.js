f6b2b1ad5477fa31dae58e0ae1baca388261ae5e
elias <eliasbg2000@gmail.com>
2025-01-03 19:04:51 +0100

    tema oscuro hecho

---
+++ js/theme.js
@@ -0,0 +1,23 @@
+// Seleccionamos el switch y el icono
+const themeToggle = document.getElementById("themeToggle");
+const themeIcon = document.getElementById("themeIcon");
+
+// Verificamos el tema al cargar la página
+if (localStorage.getItem("theme") === "dark") {
+  document.body.classList.add("dark-mode");
+  themeToggle.checked = true;
+  themeIcon.classList.replace("bi-moon", "bi-sun");
+}
+
+// Cambiar el tema cuando se hace el toggle
+themeToggle.addEventListener("change", () => {
+  if (themeToggle.checked) {
+    document.body.classList.add("dark-mode");
+    themeIcon.classList.replace("bi-moon", "bi-sun");
+    localStorage.setItem("theme", "dark");
+  } else {
+    document.body.classList.remove("dark-mode");
+    themeIcon.classList.replace("bi-sun", "bi-moon");
+    localStorage.setItem("theme", "light");
+  }
+});
