15473879fdbf9acc3e6a2bc0ad83cbeb54f103f4
elias <eliasbg2000@gmail.com>
2025-01-07 15:31:28 +0100

    slider hecho

---
+++ js/swiper.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const swiper = new Swiper(".swiper-container", {
+      loop: true,
+      speed: 1000,
+      pagination: {
+        el: ".swiper-pagination",
+        type: "progressbar",
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      autoplay: {
+        delay: 5000
+      },
+      effect: "coverflow",
+      coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+      },
+      keyboard: {
+        enabled: true,
+        onlyInViewport: true,
+      },
+      mousewheel: {
+        invert: true,
+      },
+    });
+  });
+  
